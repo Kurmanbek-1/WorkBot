bfe4b86a7068ac138d4f57e787038491e4e0366e
JuniorTeam-1 <junior.dev.k1@gmail.com>
2024-02-02 02:36:59 +0600

    рабочая ветка

--- config.py
+++ config.py
@@ -3,10 +3,9 @@ from decouple import config
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from db.db_psql.db_osor import Database
 
-
 storage = MemoryStorage()
 
-TOKEN = "6044276837:AAGhVb_j6ODq0zFMZjCLopMIcFM3NZ3SF4A"
+TOKEN = config("TOKEN")
 
 Admins = [995712956, ]
 
@@ -15,13 +14,6 @@ bot = Bot(TOKEN)
 
 dp = Dispatcher(bot=bot, storage=storage)
 
-
-# ip = config('ip')
-# PostgresUser = config('PostgresUser')
-# PostgresPassword = config('PostgresPassword')
-# DATABASE = config('DATABASE')
-
 POSTGRES_URL = "postgresql://postgres:123@127.0.0.1:5432/osor_tg_bot"
 DESTINATION = config('DESTINATION')
 data_b = Database(POSTGRES_URL)
-

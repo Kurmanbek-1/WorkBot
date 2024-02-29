4c7ef5ce60581d6946ae5978556e361f99501846
Kurmanbek-1 <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>
2024-02-29 13:52:06 +0600

    update

--- config.py
+++ config.py
@@ -3,18 +3,21 @@ from decouple import config
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from db.db_psql.db_osor import Database
 
-TOKEN = "6570711981:AAGo0p436G80I_ziI24xTQblz3Z-LhPnM5w"
+
+# =============================================================
+# Токен для тестов
+# TOKEN = "6570711981:AAGo0p436G80I_ziI24xTQblz3Z-LhPnM5w"
+# =============================================================
+
 
 storage = MemoryStorage()
-# TOKEN = config('TOKEN')
+TOKEN = config('TOKEN')
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 
 
-Director = [6451475162, 1738805992, 995712956, ]
-
-Admins = [995712956, 1000541805, 958938518, 6127093234, ]
-
+Director = [6451475162, 1738805992, ]
+Admins = [995712956, ]
 Developers = [995712956, ]
 
 
@@ -24,6 +27,5 @@ PostgresPassword = config('PostgresPassword')
 DATABASE = config('DATABASE')
 
 POSTGRES_URL = f"postgresql://{PostgresUser}:{PostgresPassword}@{ip}/{DATABASE}"
-# POSTGRES_URL = "postgresql://postgres:123@postgres_compass:5432/osor_tg_bot"
 DESTINATION = "/app/media"
 data_b = Database(POSTGRES_URL)
